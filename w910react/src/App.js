16710b0b96c9a362038e327a14a3dce22f6b6de2
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-24 15:45:58 +0100

    W9D1 made Paragraph component

--- w910react/src/App.js
+++ w910react/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Paragraph from './components/Paragraph'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Paragraph />
+    
     </div>
   );
 }
