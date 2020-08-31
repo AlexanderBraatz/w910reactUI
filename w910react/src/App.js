f3abd6e852733b7a3fbf8e715b33c0f5868c566f
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-31 16:19:33 +0100

    setup for deployment, added hompage property and predeploy and deploy scripts to package.jason. also changed the Router to HashRouter

--- w910react/src/App.js
+++ w910react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch} from 'react-router-dom'; // changed BrowserRouter to HashRouter for deployment on GitHub pages ,also made changes to package.json 
 import './App.css';
 
 
